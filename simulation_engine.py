30e6b39f87e744055c4f8c7a96857562ad9b6432
FaConKnight <chanatip35@gmail.com>
2025-05-28 16:41:14 +0700

    update

--- simulation_engine.py
+++ simulation_engine.py
@@ -1,16 +1,19 @@
-
 from world_v3 import World
+from entities_v3 import AIEntity
 import export_log
 
 class SimulationEngine:
     def __init__(self):
         self.world = World()
         self.running = False
+        self.initialize_entities()
+
+    def initialize_entities(self):
+        initial_traits = ["Seekra", "Vanta", "Thesa", "Myron", "Kael"]
+        for trait in initial_traits:
+            entity = AIEntity(name=trait, core_trait=trait)
+            self.world.add_entity(entity)
 
     def step(self):
         self.world.step()
-        export_log.export_population(self.world.time.get_time(), self.world.entities)
         return self.world.summary()
-
-    def get_time(self):
-        return self.world.time.get_time()
